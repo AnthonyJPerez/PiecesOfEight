c990f87ffe4c1fb98fce91360344be4d2e20dea7
Brixican <holy.crap.its.aj@gmail.com>
2012-02-21 23:58:32 -0500

    updated CSS and images, added slidejs gallery

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -17,18 +17,20 @@
 	</head>
 	
 	<body>
-		<div class="container shadow_right" id="page">
+		<div class="container" id="page">
 			<div id="header">
 				<div id="logo">
-					<?php echo CHtml::image(Yii::app()->request->baseUrl . '/images/logo.jpg', 'Pieces of Eight Costumes by Sue LLC logo', array('width' => '150px', 'height' => '150px')); ?>
+					<a href="<?php echo $this->createUrl('site/index'); ?>">
+						<?php echo CHtml::image(Yii::app()->request->baseUrl . '/images/logo.png', 'Pieces of Eight Costumes by Sue LLC logo', array('width' => '150px', 'height' => '150px')); ?>
+					</a>
 					<div><?php echo CHtml::encode(Yii::app()->name); ?></div>
 				</div>
 				
 				<div id="top_right_nav">
 					<?php $this->widget('zii.widgets.CMenu',array(
 						'items'=>array(
-							array('label'=>'Shopping Bag (empty)', 	'url'=>array('/site/index')),
-							array('label'=>'Checkout', 			'url'=>array('/site/comment')),
+							array('label'=>'Shopping Bag (empty)', 	'url'=>array('/cart/index')),
+							array('label'=>'Checkout', 			'url'=>array('/cart/checkout')),
 							),
 					)); ?>
 				</div>
@@ -37,7 +39,7 @@
 					<?php $this->widget('zii.widgets.CMenu',array(
 						'items'=>array(
 							array('label'=>'Home', 		'url'=>array('/site/index')),
-							array('label'=>'Feedback', 	'url'=>array('/site/comment')),
+							array('label'=>'Feedback', 	'url'=>array('/site/comments')),
 							array('label'=>'About', 	'url'=>array('/site/about')),
 							array('label'=>'Resources',	'url'=>array('/site/resources')),
 							array('label'=>'Contact Us', 	'url'=>array('/site/contact')),
@@ -64,24 +66,22 @@
 					
 					<?php $this->widget('zii.widgets.CMenu',array(
 						'items'=>array(
-							array('label'=>'Accessories', 'url'=>array('/site/index')),
-							array('label'=>'Blouses', 	'url'=>array('/site/comment')),
-							array('label'=>'Capes', 	'url'=>array('/site/about')),
-							array('label'=>'Coats', 	'url'=>array('/site/contact')),
-							array('label'=>'Dresses',	'url'=>array('/site/index')),
-							array('label'=>'Pants', 	'url'=>array('/site/comment')),
-							array('label'=>'Skirts', 	'url'=>array('/site/about')),
-							array('label'=>'Tabbards', 	'url'=>array('/site/contact')),
-							array('label'=>'Vests', 	'url'=>array('/site/index')),
+							array('label'=>'New Arrivals','url'=>$this->createUrl('product/view', array('category'=>'new'))),
+							array('label'=>'Accessories', 'url'=>$this->createUrl('product/view', array('category'=>'accessories'))),
+							array('label'=>'Blouses', 	'url'=>$this->createUrl('product/view', array('category'=>'blouses'))),
+							array('label'=>'Capes', 	'url'=>$this->createUrl('product/view', array('category'=>'capes'))),
+							array('label'=>'Coats', 	'url'=>$this->createUrl('product/view', array('category'=>'coats'))),
+							array('label'=>'Dresses',	'url'=>$this->createUrl('product/view', array('category'=>'dresses'))),
+							array('label'=>'Pants', 	'url'=>$this->createUrl('product/view', array('category'=>'pants'))),
+							array('label'=>'Skirts', 	'url'=>$this->createUrl('product/view', array('category'=>'skirts'))),
+							array('label'=>'Tabbards', 	'url'=>$this->createUrl('product/view', array('category'=>'tabbards'))),
+							array('label'=>'Vests', 	'url'=>$this->createUrl('product/view', array('category'=>'vests'))),
 							),
 					)); ?>
 				</div>
 			
 				<?php echo $content; ?>
 			</div>
-			
-		
-			<div class="clear"></div>
 		
 		
 			<div id="footer">
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,6 +1,62 @@
-<?php $this->pageTitle=Yii::app()->name; ?>
+<?php 
+	// Include the jquery library
+	Yii::app()->clientScript->registerCoreScript('jquery');
+
+	// Include the slidejs gallery
+	Yii::app()->clientScript->registerScriptFile( 
+		Yii::app()->request->baseUrl . '/js/slides.jquery.js', 
+		CClientScript::POS_HEAD
+	);
+	
+	// Include the slidejs gallery css file
+	Yii::app()->clientScript->registerCssFile(
+		Yii::app()->request->baseUrl . '/css/slidejs_gallery.css',
+		'screen'
+	);
+	
+	Yii::app()->clientScript->registerScript(
+		'SlideJS_Gallery',
+		'
+		$("#slides").slides(
+		{
+			preload: true,
+			preloadImage: "' . Yii::app()->request->baseUrl . '/images/test/loading.gif",
+			play: 3000,
+			pause: 1000,
+			hoverPause: true
+		});
+		',
+		CClientScript::POS_READY
+	);
+	
+	$this->pageTitle=Yii::app()->name;
+?>
+
+
 
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+
+
+<!-- Gallery -->
+<div id="container">
+	<div id="example">
+		<div id="slides">
+			<div class="slides_container">
+				<a href="" title="145.365 - Happy Bokeh Thursday! | Flickr - Photo Sharing!" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/slide-1.jpg" width="570" height="270" alt="Slide 1"></a>
+				<a href="" title="Taxi | Flickr - Photo Sharing!" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/slide-2.jpg" width="570" height="270" alt="Slide 2"></a>
+				<a href="" title="Happy Bokeh raining Day | Flickr - Photo Sharing!" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/slide-3.jpg" width="570" height="270" alt="Slide 3"></a>
+				<a href="" title="We Eat Light | Flickr - Photo Sharing!" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/slide-4.jpg" width="570" height="270" alt="Slide 4"></a>
+				<a href="" title="“I must go down to the sea again, to the lonely sea and the sky; and all I ask is a tall ship and a star to steer her by.” | Flickr - Photo Sharing!" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/slide-5.jpg" width="570" height="270" alt="Slide 5"></a>
+				<a href="" title="twelve.inch | Flickr - Photo Sharing!" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/slide-6.jpg" width="570" height="270" alt="Slide 6"></a>
+				<a href="" title="Save my love for loneliness | Flickr - Photo Sharing!" target="_blank"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/slide-7.jpg" width="570" height="270" alt="Slide 7"></a>
+			</div>
+			<a href="#" class="prev"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/arrow-prev.png" width="24" height="43" alt="Arrow Prev"></a>
+			<a href="#" class="next"><img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/arrow-next.png" width="24" height="43" alt="Arrow Next"></a>
+		</div>
+		<img src="<?php echo Yii::app()->request->baseUrl; ?>/images/test/example-frame.png" width="739" height="341" alt="Example Frame" id="frame">
+	</div>
+</div>
+
 
 <p>Congratulations! You have successfully created your Yii application.</p>
 
@@ -14,3 +70,18 @@
 the <a href="http://www.yiiframework.com/doc/">documentation</a>.
 Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
 should you have any questions.</p>
+
+
+
+
+
+
+
+
+
+
+		
+	
+	
+	
+	
