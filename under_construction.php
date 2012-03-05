8aa6fd6832014a53c273eeba42e2b9788da506f8
unknown <aperez@HHAUGLAND-PC.UTB.local>
2012-03-05 11:51:57 -0500

    Created Under Construction Page.

--- under_construction.php
+++ under_construction.php
@@ -1,18 +1,90 @@
 <html>
 	<head>
 		<style type='text/css' media='all'>
-			body {
-				background-color: #000;
-				background-image: url('images/bg_noise_black.png?r=1');
-				background-repeat: repeat;
-				color: white;
+			* { margin, padding: 0; }
+			
+			body 
+			{
+				background-image: url('images/bg_noise_grey.gif');
+				background-repeat:repeat;
+				margin: 1em;
+				font-family: Calibri, Helvetica, Arial, sans-serif;
+				font-size: 27pt;
+				font-weight: bold;
+			}
+			
+			h1 { font-size: 80pt;  }
+			h2 { font-size: 50pt; }
+			
+			a
+			{
+				color: #bb0000;
+				text-decoration: none;
+			}
+			
+			a:hover
+			{
+				color: #dd0000;
+				text-shadow: 0px 1px 4px #000;
+			}
+			
+			#content
+			{
+				text-align: center;
+				width: 800px;
+				margin: 0 auto;
+			}
+			
+			.letterpress
+			{
+				color: #333;
+				text-shadow: 0px 2px 3px #bbb;
+			}
+			
+			p
+			{
+				position: relative;
+				margin: 0 auto;
+				width: 80%;
+				height: 200px;
+				background-color: #333;
+				color: #ccc;
+				padding: 20px;
+				text-shadow: 0px 2px 3px #000;
+			
+				-webkit-box-shadow: 0px 3px 4px #ccc;
+				-moz-box-shadow: 0px 3px 4px #ccc;
+				box-shadow: 0px 3px 4px #ccc;
+				
+				-webkit-border-radius: 10px;
+				-moz-border-radius: 10px;
+				border-radius: 10px;
+			}
+			
+			p > span
+			{
+				position: absolute;
+				right: 12px;
+				bottom: 10px;
+				font-size: 16pt;
 			}
 		</style>
+		
+		<title>Pieces of Eight Costumes by Sue LLC</title>
 	</head>
 	<body>
-		<p>
-			This site is currently under construction! In the meantime, check 
-			out our <a href='http://www.etsy.com/shop/piecesof8costumes'>Etsy Shop</a>
-		</p>
+	
+		<div id='content'>
+			<img src='images/logo3.png' alt='Pieces of Eight Costumes by Sue LLC logo' />
+			<h1 class='letterpress'>Avast Ye!</h1>
+			<h2 class='letterpress'>This page be Under Construction.</h2>
+			<p>
+				Aye, we be down for now, but if ye be
+				going on the account, ye'd do good to drop anchor in our 
+				<a href='http://www.etsy.com/shop/piecesof8costumes'>Etsy Shop</a> 
+				while we batten down the hatches and weigh anchor.
+				<span>PiecesOf8Costumes@comcast.net</span>
+			</p>
+		</div>
 	</body>
 </html>
