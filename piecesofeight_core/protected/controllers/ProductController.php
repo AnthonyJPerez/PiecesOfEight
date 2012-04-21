a22d5210f9d28a17969d40c2de3a7e8425a52781
Brixican <holy.crap.its.aj@gmail.com>
2012-04-21 03:52:09 -0400

    Created an xml feed for Googles Product Search

--- piecesofeight_core/protected/controllers/ProductController.php
+++ piecesofeight_core/protected/controllers/ProductController.php
@@ -36,6 +36,84 @@ class ProductController extends GxController
 	}
 	
 	
+	// Used to generate XML specific for Google's product feed
+	public function actionGoogleProductFeed()
+	{
+		$products = Product::model()->with('images', 'p8Sizes')->findAll();
+		header('Content-Type: text/xml');
+		echo '<?xml version="1.0"?>';
+		echo '<feed xmlns="http://www.w3.org/2005/Atom" xmlns:g="http://base.google.com/ns/1.0" xmlns:c="http://base.google.com/cns/1.0">';
+	
+		echo '<id>tag:piecesofeightcostumes.com,2012-04-21:/index.php?r=product/googleProductFeed</id>';
+		echo '<title>Pieces of Eight Product Listings</title>';
+		echo '<link href="http://piecesofeightcostumes.com" rel="alternate" type="text/html" />';
+		echo '<updated>2012-04-21T02:46:00Z</updated>';
+		echo '<author><name>Pieces of Eight Costumes</name></author>';
+	
+		// Generate each product:
+		foreach ($products as $product)
+		{
+			echo '<entry>';
+				echo '<g:id>po8_'.$product->id.'</g:id>';
+				echo '<title>'.$product->name.'</title>';
+				echo '<description>'.$product->description.'</description>';
+				echo '<g:google_product_category>Apparel &amp; Accessories &gt; Costumes &amp; Accessories &gt; Costumes</g:google_product_category>';
+				echo '<link>http://piecesofeightcostumes.com/index.php?r=product/view&amp;id='.$product->id.'</link>';
+				
+				// Update later with default picture
+				$count = 0;
+				foreach ($product->images as $img)
+				{
+					if ($count < 1)
+					{
+						$tag = 'g:image_link';
+					}
+					else
+					{
+						$tag = 'g:additional_image_link';
+					}
+					echo '<'.$tag.'>http://piecesofeightcostumes.com/images/product-images/'.$img->url.'</'.$tag.'>';
+					$count++;
+				}
+				echo '<g:condition>new</g:condition>';
+				echo '<g:availability>in stock</g:availability>';
+				echo '<g:price>'.$product->price.' USD</g:price>';
+				// echo '<g:sale_price></g:sale_price>';
+				echo '<g:brand>Pieces of Eight Costumes</g:brand>';
+				
+				// Update when products specify their gender
+				echo '<g:gender>unisex</g:gender>';
+				echo '<g:age_group>Adult</g:age_group>';
+				
+				// Update when products specify their colors
+				echo '<g:color>Black</g:color>';
+				echo '<g:color>White</g:color>';
+				echo '<g:color>Grey</g:color>';
+				echo '<g:color>Burgandy</g:color>';
+				echo '<g:color>Purple</g:color>';
+				echo '<g:color>Brown</g:color>';
+				
+				foreach ($product->p8Sizes as $size)
+				{
+					echo '<g:size>'.$size->size.'</g:size>';
+				}
+				
+				// Update if shipping info changes:
+				echo '<g:shipping>';
+					echo '<g:country>US</g:country>';
+					echo '<g:service>Ground</g:service>';
+					echo '<g:price>8.95 USD</g:price>';
+				echo '</g:shipping>';
+				
+			echo '</entry>';
+		}
+		
+		echo '</feed>';
+		exit();
+	
+	}
+	
+	
 	public function actionList($category=null)
 	{	
 		// Find the category id
