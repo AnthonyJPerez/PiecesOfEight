4b1cce04a53a8d1bb4b18115fb807e703c933b43
Brixican <holy.crap.its.aj@gmail.com>
2012-05-17 23:13:13 -0400

    Removed size chart from product creation

--- piecesofeight_core/protected/models/Product.php
+++ piecesofeight_core/protected/models/Product.php
@@ -24,7 +24,7 @@ class Product extends BaseProduct
 	public function rules()
 	{
 		return array(
-			array('name, price, category_id, description, size_chart, care_information', 'required'),
+			array('name, price, category_id, description, care_information', 'required'),
 			array('name', 'length', 'max'=>75),
 			array('name,description,size_chart,care_information', 'type', 'type'=>'string'),
 			array('name', 'unique', 'className' => 'Product'),
@@ -32,8 +32,8 @@ class Product extends BaseProduct
 			array('price', 'type', 'type'=>'float'),
 			array('category_id', 'exist', 'attributeName'=>'id', 'className'=>'Category'),
 			//array('default_image_id', 'exist', 'attributeName'=>'id', 'className'=>'Image'), <-- can use this, but must support null as well
-			array('date_inserted, description, size_chart, care_information', 'safe'),
-			array('date_inserted, description, size_chart, care_information', 'default', 'setOnEmpty' => true, 'value' => null),
+			array('date_inserted, description, care_information', 'safe'),
+			array('date_inserted, description, care_information', 'default', 'setOnEmpty' => true, 'value' => null),
 		);
 	}
 	
