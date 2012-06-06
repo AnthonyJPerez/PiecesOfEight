dabeca2ed2aae98131acf3bba4e2de1fa4db6e21
Brixican <holy.crap.its.aj@gmail.com>
2012-06-05 20:35:49 -0400

    Edited index.php by setting debug mode to false in production mode. Custom order page now only displays in debug mode.

--- piecesofeight_core/protected/views/product/custom.php
+++ piecesofeight_core/protected/views/product/custom.php
@@ -169,6 +169,13 @@
 	This page is currently in development. For custom order inquiries, check out our <?php echo CHtml::link('Contact Page', $this->createUrl('site/contact')); ?>
 </p>
 
+
+<?php
+	// TEmporary!! Only have this code here until this page is ready for production mode
+	if (defined('YII_DEBUG') && constant('YII_DEBUG') != false)
+	{
+?>
+		
 <h2>How to place your custom order:</h2>
 
 <ol class='instructions'>
@@ -247,3 +254,7 @@
 	
 	</ul>
 </div>
+
+<?php
+}	// end debug mode
+?>
