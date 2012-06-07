153adcf9ce12050246bd717c0b123ea5bae6bc0c
Brixican <holy.crap.its.aj@gmail.com>
2012-06-06 22:57:06 -0400

    Updated partners listings.

--- piecesofeight_core/protected/views/layouts/main.php
+++ piecesofeight_core/protected/views/layouts/main.php
@@ -1,12 +1,13 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
+<!-- DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd" -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<!-- SEO Meta tags -->
 		<!-- Safe-Surf metatag -->
 		<meta http-equiv="PICS-Label" content='(PICS-1.1 "http://www.classify.org/safesurf/" L gen true for "http://www.piecesofeightcostumes.com/" r (SS~~000 1))' />
-		<meta name="netinsert" content="0.0.1.12.10.1">
+		<meta name="netinsert" content="0.0.1.12.10.1" />
 		
-		<link rel="shortcut icon" type="image/x-icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon.ico">
+		<link rel="shortcut icon" type="image/x-icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon.ico" />
 		
 		<?php
 			if ($this->pageCanonical !== null)
@@ -17,8 +18,8 @@
 			}
 		?>
 
-		<meta name="language" content="en" />
-		<meta name="viewport" content="width=device-width; initial-scale=1.0">
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+		<meta http-equiv="Content-Language" Content="en" />
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<meta name="copyright" content="Copyright &copy;, <?php echo date('Y'); ?> Peices Of Eight Costumes by Sue LLC. All rights reserved." />
 		<meta name="author" content="Pieces of Eight Costumes" />
