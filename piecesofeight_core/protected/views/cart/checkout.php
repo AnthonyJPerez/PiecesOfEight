f6bb71d26dca06fbb7be1684eb5ef72bce6e05aa
Anthony Perez <holy.crap.its.aj@gmail.com>
2012-07-26 23:08:54 -0400

    Checkout page now displays the users order number.

--- piecesofeight_core/protected/views/cart/checkout.php
+++ piecesofeight_core/protected/views/cart/checkout.php
@@ -4,8 +4,13 @@
 <h1>Ahoy! Thanks for Purchasing</h1>
 
 <p>
-	Aye, thank you for purchasing from Pieces of Eight Costumes!  We have received yer order. 
-	Ye shall receive an email shortly containing yer invoice and estimated shipping information.
+	Aye, thank you for purchasing from Pieces of Eight Costumes!  Your order number is:
+</p>
+<p>
+	<b><?php echo $confirmCode; ?></b>
+</p>
+<p>
+	We have received yer order. Ye shall receive an email shortly containing yer invoice and estimated shipping information.
 </p>
 <p>
 	Have question about your order? <?php echo CHtml::link('Email us here', $this->createUrl('site/contact'), array('title'=>'Questions about your Pieces of Eight Costumes Order')); ?>
