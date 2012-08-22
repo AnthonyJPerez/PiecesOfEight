50b30b930b41ff5b1baa3dd2363daae700b5b12a
Anthony Perez <Holy.Crap.Its.AJ@gmail.com>
2012-08-21 20:20:48 -0400

    Products that are not shippable are now hidden from the products page

--- piecesofeight_core/protected/views/product/_view.php
+++ piecesofeight_core/protected/views/product/_view.php
@@ -1,24 +1,34 @@
 <?php 
 	/* This view gets applied for each product in the list */
+	
+	// @hack @todo (remove this eventually).
+	// Some products are not actually "Products," but rather add-ons. 
+	// These products should not show up in the normal product listings, unless the category is
+	// "Miscellaneous". Skip rendering of these products.
+	if ( $data->shippable == 1 || ucfirst($category) == ucfirst("miscellaneous") )
+	{
 ?>
-<div class="view background_shadow">
-	<a href="<?php echo $data->getUrl(); ?>">
-		<?php 
-			$defaultImage = $data->getDefaultImage();
-			echo CHtml::image(
-				Yii::app()->request->baseUrl . '/images/product-images/' . $defaultImage->url,
-				$data->getProductImgAltDescription()
-			);
-		?>
-	</a>
-	
-	<div class='product_name'>
+	<div class="view background_shadow">
 		<a href="<?php echo $data->getUrl(); ?>">
-			<?php echo $data->name; ?>
+			<?php 
+				$defaultImage = $data->getDefaultImage();
+				echo CHtml::image(
+					Yii::app()->request->baseUrl . '/images/product-images/' . $defaultImage->url,
+					$data->getProductImgAltDescription()
+				);
+			?>
 		</a>
+		
+		<div class='product_name'>
+			<a href="<?php echo $data->getUrl(); ?>">
+				<?php echo $data->name; ?>
+			</a>
+		</div>
+		
+		<div class='product_price'>
+			<?php echo '$' . $data->price; ?>
+		</div>
 	</div>
-	
-	<div class='product_price'>
-		<?php echo '$' . $data->price; ?>
-	</div>
-</div>
+<?php
+	}
+?>
