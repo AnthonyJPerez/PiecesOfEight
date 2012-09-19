7df8a9103b2d5c51a25c7ba1ebcc6d2549e76005
Anthony Perez <Holy.Crap.Its.AJ@gmail.com>
2012-09-18 20:31:13 -0400

    Created admin home page
    Added breadcrumbs to existing admin pages

---
+++ piecesofeight_core/protected/views/admin/orders.php
@@ -0,0 +1,45 @@
+<?php 
+	// Main Admin Page
+?>
+
+<div id="breadcrumbs">
+	<ul>
+		<li>
+			<?php echo CHtml::link(
+					'Admin Home',
+					$this->createUrl('admin/index')
+				);
+			?>
+		</li>
+		<li>
+			> Orders
+		</li>
+	</ul>
+</div>
+
+
+<h1>
+	Manage Orders
+</h1>
+
+
+<div>
+	
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+		
+	
+	
+	
+	
