c5806ceed2d884e500cab933d0173ced72766081
Anthony Perez <Holy.Crap.Its.AJ@gmail.com>
2012-09-18 22:33:01 -0400

    Completed order management system

--- piecesofeight_core/protected/controllers/AdminController.php
+++ piecesofeight_core/protected/controllers/AdminController.php
@@ -36,8 +36,58 @@ class AdminController extends GxController
 	}
 
 
+	function actionOrder($id)
+	{
+		$order = Order::model()->findByPk($id);
+
+		if (isset($_POST['Order'])) 
+		{
+			$order->order_status = $_POST['Order']['order_status'];
+			$order->save();
+			
+			$this->redirect(array('admin/orders'));
+		}
+
+		$this->render(
+			'order',
+			array(
+				'order' => $order
+			)
+		);
+	}
+
+
+	// @todo: Would be nice in the future to track when orders are shipped, etc..
 	function actionOrders()
 	{
-		$this->render('orders');
+		$orders_list = Order::model()->findAll(
+			array(
+				'order' => 'order_date ASC'
+			)
+		);
+
+		$orders = array();
+
+		// Setup the orders to have an entry for each order state
+		foreach (array('open','active','ready','shipped','completed') as $enum_value)
+		{
+			$orders[$enum_value] = array();
+		}
+
+		// Fill in the orders array
+		foreach ($orders_list as $order)
+		{
+			array_push($orders[$order->order_status], $order);
+		}
+
+		// Reverse the order of the completed list
+		$orders['completed'] = array_reverse($orders['completed']);
+
+		$this->render(
+			'orders',
+			array(
+				'orders' => $orders
+			)
+		);
 	}
 }
--- piecesofeight_core/protected/views/admin/orders.php
+++ piecesofeight_core/protected/views/admin/orders.php
@@ -24,7 +24,36 @@
 
 
 <div>
-	
+	<?php
+		foreach ($orders as $category => $orders)
+		{
+		?>
+			<h2><?php echo ucfirst($category) . " Orders" ?></h2>
+			<div>
+				<ul>
+					<?php
+						if (empty($orders)) {
+							echo "<li>None</li>";
+						}
+						else
+						{
+							foreach ($orders as $order)
+							{
+								echo "<li>";
+								$date = preg_split("/\s/", $order->order_date)[0];
+								echo CHtml::link(
+									"[".$date ."] " . $order->shipto_name . " -- " . $order->confirmation_code,
+									$this->createUrl('admin/order', array('id' => $order->id))
+								);
+								echo "</li>";
+							}
+						}
+					?>
+				</ul>
+			</div>
+		<?php
+		}
+	?>
 </div>
 
 
