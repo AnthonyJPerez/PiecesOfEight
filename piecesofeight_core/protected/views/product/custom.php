a30e4c2a4282ad37c0514054eb5fa28530a79418
Anthony Perez <Holy.Crap.Its.AJ@gmail.com>
2012-09-20 13:56:53 -0400

    Added support for custom orders
    Custom orders now show up specially in the custom orders section of the
    website.

--- piecesofeight_core/protected/views/product/custom.php
+++ piecesofeight_core/protected/views/product/custom.php
@@ -3,6 +3,100 @@
 
 
 	Yii::app()->clientScript->registerCss(
+		'custom_product_view_rounded',
+		'
+			.background_shadow
+			{
+				background-color: #f7f7f7;
+				
+				-webkit-box-shadow: 3px 3px 8px -1px #666;
+				-moz-box-shadow: 3px 3px 8px -1px #666;
+				box-shadow:  3px 3px 8px -1px #666;
+			}
+			
+			#product_listing
+			{
+				padding: 1em;
+				background-color: lightgrey;
+			}
+			
+				#product_listing > img
+				{
+					width: 45%;
+				}
+			
+			.list-view
+			{
+				width: 100%;
+				padding-left: 1.5em;
+			}
+				.view {
+					position: relative;
+					
+				}
+				
+				.view img
+				{
+					width: 100%;
+					margin-bottom: 5px;
+				}
+
+				.view .product_name
+				{
+					float: left;
+					text-align: left;
+					width: 70%;	
+				}
+				
+				.view .product_price
+				{
+					position: absolute;
+					right: 5px;
+					bottom: 5px;
+				}
+				
+				.list-view > .items > .view
+				{
+					display: inline-block;
+					vertical-align: top;
+					margin: .7em;
+					padding: 0.5em;
+					width: 30%;		
+				}
+				
+			.list-view .summary
+			{
+				display: none;
+			}
+			
+			.list-view .sorter
+			{
+				margin-right: 1.5em;
+			}
+			
+			.list-view .empty
+			{
+				position: absolute;
+				top: 75px;
+				margin-left: 1em;
+			}
+			
+			.list-view-loading
+			{
+				background-image: none;
+			}
+			
+			
+			.empty
+			{
+				margin-top: 2em;
+			}
+		',
+		'screen'
+	);
+
+
+	Yii::app()->clientScript->registerCss(
 		'product-custom-form-style',
 		'	
 			#product_list
@@ -407,9 +501,63 @@
 </p>
 
 
+<h2> Custom Listings </h2>
+
+
+<?php 
+
+$this->widget('zii.widgets.CListView', array(
+	'dataProvider'=>$_CustomProducts,
+	'itemView'=>'_view',
+	'enablePagination' => true
+));
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <?php
 	// Temporary!! Only have this code here until this page is ready for production mode
-	if (defined('YII_DEBUG') && constant('YII_DEBUG') != false)
+	// temporary: don't show it at all
+	$dontshow = true;
+	if (!$dontshow && defined('YII_DEBUG') && constant('YII_DEBUG') != false)
 	{
 ?>
 		
