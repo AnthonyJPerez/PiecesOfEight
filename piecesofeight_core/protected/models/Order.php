c6b6b7381f87c5452e8479943bf27ad6ae9c1211
Anthony Perez <Holy.Crap.Its.AJ@gmail.com>
2012-09-19 23:14:22 -0400

    Added feedback form inserter. Updated select2 js library, converted feedback to read from database.

--- piecesofeight_core/protected/models/Order.php
+++ piecesofeight_core/protected/models/Order.php
@@ -6,19 +6,5 @@ class Order extends BaseOrder
 {
 	public static function model($className=__CLASS__) {
 		return parent::model($className);
-	}
-
-	 public static function enumItem($model,$attribute)
-        {
-                $attr=$attribute;
-                CHtml::resolveName($model,$attr);
-                preg_match('/\((.*)\)/',$model->tableSchema->columns[$attr]->dbType,$matches);
-                foreach(explode(',', $matches[1]) as $value)
-                {
-                        $value=str_replace("'",null,$value);
-                        $values[$value]=Yii::t('enumItem',$value);
-                }
-                
-                return $values;
-        }   
+	}  
 }
