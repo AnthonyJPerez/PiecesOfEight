ea875e97a7f5ce94d43d1e1dabb88fbf8d168450
Anthony Perez <Holy.Crap.Its.AJ@gmail.com>
2012-10-04 21:19:09 -0400

    Updated CSS cachebuster

--- piecesofeight_core/protected/views/layouts/main.php
+++ piecesofeight_core/protected/views/layouts/main.php
@@ -15,7 +15,7 @@
 		<link rel="shortcut icon" type="image/x-icon" href="<?php echo Yii::app()->request->baseUrl; ?>/images/favicon.ico" />
 		
 		<?php
-			$cssTag = "09272012";
+			$cssTag = "10042012";
 			if ($this->pageCanonical !== null)
 			{
 				?>
